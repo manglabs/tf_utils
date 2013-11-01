494745ddaddb41fb4a87a237581ed6bbddfb70ad
Benjamin E White <benjamin.e.white@gmail.com>
2013-11-01 17:16:38 -0400

    mods to pg_backup.sh

--- heroku_pg_backup.sh
+++ heroku_pg_backup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+# to run this script you must have access privileges for Thinkful's production
+# Heroku account.  
+
 echo "Deleting existing backup"
 dropdb --if-exists tf_prod_backup
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
 
 echo "Intitializing tf_prod_backup db"
-createdb tf_prod_backup
+createdb -U postgres tf_prod_backup
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
 
 echo "Retrieving most recent pg_backup from Heroku."
@@ -13,17 +16,23 @@ curl -o ${TEMP_BACKUP_PATH} `heroku pgbackups:url --app tf-pysplash-prod`
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
 
 echo "Restoring backup to tf_prod_backup"
-pg_restore ${TEMP_BACKUP_PATH} -d tf_prod_backup --no-owner
+pg_restore --no-owner --no-acl ${TEMP_BACKUP_PATH} -d tf_prod_backup
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
+rm ${TEMP_BACKUP_PATH}
 
 echo "Sanitizing backup data. Please hold."
-psql_output=`psql tf_prod_backup << EOF
+psql_output=`psql -U postgres tf_prod_backup << EOF
 UPDATE users SET password='';
 UPDATE users SET salt='';
-UPDATE users SET tf_login='_' || tf_login;
-UPDATE contacts SET email = '_' || email;
-UPDATE contacts SET tf_login = '_' || tf_login WHERE tf_login IS NOT NULL;
+UPDATE users SET tf_login= tf_login || '_';
+UPDATE contacts SET email = email || '_';
+UPDATE contacts SET tf_login = tf_login || '_' WHERE tf_login IS NOT NULL;
 EOF`
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
 
-echo "Congratulations! Your backup worked, friend!"
+echo "Dumping sanitized database so you can share it with others"
+dump_path=$1
+pg_dump --no-owner tf_prod_backup > $dump_path"tf_backup.dump" 
+if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
+
+echo "Congratulations! Your backup worked!"
