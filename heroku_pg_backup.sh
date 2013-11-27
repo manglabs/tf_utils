ec59471471fbf8e18ba15e1115998da9d17ee1c7
Benjamin White <benjamin.e.white1@gmail.com>
2013-11-27 12:18:48 -0500

    heroku_pg_backup.sh now dumps backup to environment repo

--- heroku_pg_backup.sh
+++ heroku_pg_backup.sh
@@ -4,11 +4,11 @@
 # Heroku account.  
 
 echo "Deleting existing backup"
-dropdb --if-exists tf_prod_backup
+dropdb --if-exists tf_prod_raw_backup
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
 
 echo "Intitializing tf_prod_backup db"
-createdb -U postgres tf_prod_backup
+createdb -U postgres tf_prod_raw_backup
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
 
 echo "Retrieving most recent pg_backup from Heroku."
@@ -16,12 +16,12 @@ curl -o ${TEMP_BACKUP_PATH} `heroku pgbackups:url --app tf-pysplash-prod`
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
 
 echo "Restoring backup to tf_prod_backup"
-pg_restore --no-owner --no-acl ${TEMP_BACKUP_PATH} -d tf_prod_backup
+pg_restore --no-owner --no-acl ${TEMP_BACKUP_PATH} -d tf_prod_raw_backup
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
 rm ${TEMP_BACKUP_PATH}
 
 echo "Sanitizing backup data. Please hold."
-psql_output=`psql -U postgres tf_prod_backup << EOF
+psql_output=`psql -U postgres tf_prod_raw_backup << EOF
 UPDATE users SET password='';
 UPDATE users SET salt='';
 UPDATE users SET tf_login= tf_login || '_';
@@ -31,8 +31,7 @@ EOF`
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
 
 echo "Dumping sanitized database so you can share it with others"
-dump_path=$1
-pg_dump --no-owner --no-acl tf_prod_backup > $dump_path"tf_backup.dump" 
+pg_dump --no-owner --no-acl tf_prod_raw_backup > $FIXTURE_ROOT/postgres/tf_backup.dump 
 if [ `echo $?` -ne 0 ]; then echo "Last cmd failed! Aborting."; exit 100; fi
 
 echo "Congratulations! Your backup worked!"
